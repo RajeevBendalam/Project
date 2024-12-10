fec605434fabba791e4e774bfb176ca58f02f8de
RajeevBendalam <145048736+RajeevBendalam@users.noreply.github.com>
2024-12-11 05:00:52 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "${process.env.CLIENT_URL}/auth/google/callback",
+      callbackURL: "http://localhost:3000/auth/google/callback",
       passReqToCallback: true
     },
     async function(request, accessToken, refreshToken, profile, done) {

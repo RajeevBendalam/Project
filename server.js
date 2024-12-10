753a39df9607b6d075f9efc74bac7a2bb3b1d494
RajeevBendalam <145048736+RajeevBendalam@users.noreply.github.com>
2024-12-11 04:20:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/');
+  res.redirect('${process.env.CLIENT_URL}/');
 }
 
 // Routes

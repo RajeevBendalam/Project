15da1f7df3f7dbdfdd9a6cfeae5d78beae05f37b
RajeevBendalam <145048736+RajeevBendalam@users.noreply.github.com>
2024-12-11 20:20:22 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://project-jp4c.onrender.com/auth/google/callback",
+      callbackURL: "https://project-gamma-mocha-34.vercel.app/auth/google/callback",
       passReqToCallback: true
     },
     async function(request, accessToken, refreshToken, profile, done) {
